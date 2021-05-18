b9a741ee73fef8a60362127f79dc33d3daa6db42
devikaSukumaran <devikasukumaran507@gmail.com>
2021-05-18 20:39:54 +0100

    Adding unit tests

--- IconAppTests/IconViewModelTests.swift
+++ IconAppTests/IconViewModelTests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-class IconAppTests: XCTestCase {
+class IconViewModelTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -28,5 +28,4 @@ class IconAppTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
